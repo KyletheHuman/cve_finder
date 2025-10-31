dfde3a655bba807bb1afa64773666a0afa49e960
adam-crow <adam.thecrows@gmail.com>
2025-10-31 18:17:27 -0400

    guideline of what to get done in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 
 #include "trie.h"
 
+// yoink mass data dumps for each year of cve's
+//push all data into both trees
+//parse inputs
+//time search for both inputs
+//print list
+// *after due optimize to use better structure*
 void main() {
     return;
 }
