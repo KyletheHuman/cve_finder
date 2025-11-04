909a3486cba7d93926caf34b2e683e4ac7b6ef47
adam-crow <adam.thecrows@gmail.com>
2025-11-03 23:44:56 -0500

    template todo

---
+++ src/api_processor.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "cve_struct.h"
+using namespace std;
+
+
+//handle download, parse, logic
+  //update vs load
+  //download
+  //decompress
+  //parse
+  //save data
+
