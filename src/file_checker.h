b52ed349ae33a9baaf36110c9d562c9725c0e02c
adam-crow <adam.thecrows@gmail.com>
2025-11-03 20:32:18 -0500

    Create file_checker.h + template

---
+++ src/file_checker.h
@@ -0,0 +1,7 @@
+//file exists at given path ot data allcvs? checkFile
+
+//clean up string to be consistent in searches cleanInput
+//remove quotes
+//lowercase
+//remove spaces between version and software with -
+//keep only letters and nums
