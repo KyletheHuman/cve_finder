2cc2a81274b7522623d7276d6a63b02d995bc02e
adam-crow <adam.thecrows@gmail.com>
2025-11-04 19:46:00 -0500

    Updated to new searching method

--- src/cve_struct.h
+++ src/cve_struct.h
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string> //just in case
+using namespace std;
 
 struct CVEstruct {
   string id;
   string description;
   double cvss3score;
-  string productversion;
+  string vendor;
+  string product;
+  string version;
 
   void print() {
         cout << "ID: " << id << endl;
+        cout << "CVSS3.0 score: " << cvss3score << endl;
         cout << "Description: " << description << endl;
     }
 };
