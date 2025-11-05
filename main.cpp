0f7f672aa6892187f7e0f8f7d65191b58906543f
adam-crow <adam.thecrows@gmail.com>
2025-11-04 19:25:19 -0500

    Create main.cpp + template

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "cve_struct.h"
+#include "api_processor.h"
+#include "file_checker.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main (int argc, char* argv[]) {
+  cout << "CVE Finder" << endl;
+
+  vector<CVEstruct> cves; //stores all cves
+
+  // cout to user how to format search
+  
+  //user input argument handling
+
+  //data loading
+
+  //user inputs loop
+
+  //searches and printing out
+
+  //time tree searches
+
+}
