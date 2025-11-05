d84dfb0f37e0df80ecd63ec777fc01a19e8a37e0
adam-crow <adam.thecrows@gmail.com>
2025-11-05 14:05:15 -0500

    Updated commands (partial search)
    still need time and tree insert and others

--- main.cpp
+++ main.cpp
@@ -9,19 +9,79 @@ using namespace std;
 
 int main (int argc, char* argv[]) {
   cout << "CVE Finder" << endl;
+  cout << "commands: update, load, search, exit" << endl;
 
   vector<CVEstruct> cves; //stores all cves
+  string command;
+
+  while(true) {
+    getline(cin, command);
+    command = cleanInput(command);
+    
+    if (command = "exit") {
+      break;
+      
+    } else if (command == "update") {
+      cout << "Updating CVE data" << endl;
+      updateData();
+      cves = loadData();
+      
+    } else if (command = "load") {
+      cout << "Loading CVE data" << endl;
+      cves = loadData();
+      if (cves.empty()) {
+        cout << "No local data. Try update" << endl;
+      }
+      
+    } else if (command == "search") {
+      if (cves.empty()) {
+        cout << "No local data. Try load or update" << endl;
+        continue;
+      }
+      
+      string vendor;
+      string product;
+      string version;
+      
+      cout << "Search - leave blank for any except product" << endl;
+      cout << "Enter vendor:  ";
+      getline(cin, vendor);
+      vendor = cleanInput(vendor);
+      
+      cout << "Enter product*:  ";
+      getline(cin, product);
+      if (product.empty()) {
+        while(product.empty()) {
+          cout << "Error: product can not be left empty" << endl;
+          cout << "Enter product*:  ";
+          getline(cin, product);
+        }
+      }
+      product = cleanInput(product);
+      
+      cout << "Enter version:  ";
+      getline(cin, version);
+      version = cleanInput(version);
+
+      cout << "Searching CVEs" << endl;
+      int count = 0;
+      //figure out how to search them in the trees
+
+      if (count == 0) {
+        cout << "No matching CVEs found" << endl;
+      } else {
+        //prints
+      }
+      
+    } else {
+      cout << "No command found" << endl;
+    }
+  }
 
   // cout to user how to format search
   
-  //user input argument handling
-
-  //data loading
-
-  //user inputs loop
-
   //searches and printing out
 
   //time tree searches
-
+  return 0;
 }
