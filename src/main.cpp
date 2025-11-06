52553e38c0847f8c716414c626fb820c0d73d73f
kyleg <kylegoodpaster5@gmail.com>
2025-11-06 17:07:50 -0500

    added search for trie

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,175 @@
-#include <iostream>
-
-#include "trie.h"
-#include "file_checker.h"
+#include "cve_struct.h"
 #include "data_processor.h"
+#include "file_checker.h"
+#include "trie.h"
 
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+
+
+bool checkMatch(const string &fromData, const string &fromSearch) {
+  if (fromSearch == "") {
+    return true;
+  }
+  if (fromData == "") {
+    return true;
+  }
+  return fromSearch == fromData;
+}
+
+void printCVE(const CVEstruct &cve, const string &vendorInput, const string &versionInput) {
+    cout << "--------------------" << endl;
+    cout << "ID:  " << cve.id << endl;
+    if (vendorInput.empty()) {
+        cout << "Vendor:  ";
+        if (!cve.vendor.empty())
+            cout << cve.vendor << endl;
+        else
+            cout << "any" << endl;
+    }
+    if (versionInput.empty()) {
+        cout << "Version:  ";
+        if (!cve.version.empty())
+            cout << cve.version << endl;
+        else
+            cout << "any" << endl;
+    }
+
+    cout << "CVSS3 Score:  " << cve.cvss3score << endl;
+    cout << "Description:  ";
+    if (cve.description.size() > 200) { //shortens
+        cout << cve.description.substr(0, 200) << "..." << endl;
+    } else {
+        cout << cve.description << endl;
+    }
+}
 
 string formatCPE(string vendor, string product, string version) {
     return vendor + "-" + product + "-" + version;
 }
 
-// yoink mass data dumps for each year of cve's:
-//cURL all of NVD's API feeds from each year as .gz compressed files
-//decompress into normal json
-//nholmann github json to parse
-//store each CVE entry as a struct in array
-//insert structs into trees
-//build so that search by version
-//time to search for both inputs
-//print list
-// *after due date optimize to use better structure*
-void main() {
+int main (int argc, char* argv[]) {
+  cout << "CVE Finder" << endl;
+  cout << "commands: update, load, search, exit" << endl;
 
-    //insert load results into tree and trie
-    //handle search inputs in while loop
+  vector<CVEstruct> cves; //stores all cves
+  string command;
+
+  Trie trie;
+  //Tree tree;
 
 
-    //load the data into vector
-    //give an error if there isn't anything
-    vector<CVEstruct> data = loadData();
-    if (data.empty()) {
-        cout << "Error loading data :(" << endl;
-        return;
-    }
+  while(true) {
+    getline(cin, command);
+    command = cleanInput(command);
+    
+    if (command == "exit") {
+      break;
+      
+    } else if (command == "update") {
+      cout << "Updating CVE data" << endl;
+      updateData();
+      
+    } else if (command == "load") {
+      cout << "Loading CVE data" << endl;
+      cves = loadData();
+      if (cves.empty()) {
+        cout << "No local data. Try update" << endl;
+      }
 
-    Trie trie;
-    //Tree tree;
-    for (CVEstruct cve : data) {
-        string cpe = cve.cpe();
-        trie.insert(cpe, &cve);
-        //TODO: INSERT INTO TREE
-    }
+      cout << "Building Trie" << endl;
+      for (auto &cve :cves) {
+        if (!cve.product.empty()) {
+          string cpe = cve.cpe();
+          trie.insert(cpe, &cve);
+        }
+      }
+      cout << "Trie built" << endl;
 
+      cout << "Building Red-Black Tree" << endl;
+      cout << "Red-Black Tree built" << endl;
 
-    //loop until end
-    while (true) {
-        //set mode to tree or trie
-        string mode;
-        cout << "Enter the mode (tree or trie): ";
-        cin >> mode;
-        cout << endl;
+      cout << "CVEs:  " << cves.size() << endl;
+      
+    } else if (command == "search") {
+      if (cves.empty()) {
+        cout << "No local data. Try load or update" << endl;
+        continue;
+      }
+      
+      string vendor;
+      string product;
+      string version;
+      
+      cout << "Search - leave fields vendor and/or version blank to search for all" << endl;
+      cout << "Note: do not leave product blank" << endl;
+      cout << "Enter vendor:  ";
+      getline(cin, vendor);
+      vendor = cleanInput(vendor);
+      
+      cout << "Enter product*:  ";
+      getline(cin, product);
+      if (product.empty()) {
+        while(product.empty()) {
+          cout << "Error: product can not be left empty" << endl;
+          cout << "Enter product*:  ";
+          getline(cin, product);
+        }
+      }
+      product = cleanInput(product);
+      
+      cout << "Enter version:  ";
+      getline(cin, version);
+      version = cleanInput(version);
 
-        //prompt for action
-        string choice;
-        cout << "What would you like to do (search, update, exit)? ";
-        cin >> choice;
+      cout << "Searching CVEs" << endl;
+      int count = 0;
+      //base search for now
+      // for (const auto &cve : cves) {
+      //   // if (checkMatch(cve.product, product) && checkMatch(cve.vendor, vendor) && checkMatch(cve.version, version)) {
+      //   //   printCVE(cve, vendor, version);
+      //   //   count++;
+      //   // }
+      // }
 
-        if (choice == "search") {
-            //get input for vendor, product, and version
-            string vendor, product, version;
-            cout << "Enter the software's vendor: ";
-            cin >> vendor;
-            cout << "Enter the software's product: ";
-            cin >> product;
-            cout << "Enter the software's version: ";
-            cin >> version;
-
-            if (mode == "trie") {
-                string cpe = formatCPE(vendor, product, version);
-                CPEData* result = trie.search(cpe);
-
-                if (result == nullptr) {
+      string cpe = formatCPE(vendor, product, version);
+      CPEData* result = trie.search(cpe);
+      if (result == nullptr) {
                     cout << "CPE not found." << endl;
                     continue;
                 }
                 for (CVEstruct* cve : result->cves) {
                     cve->print();
                 }
-            }
-            //TODO:: Tree
-        }
-        else if (choice == "update") {
-            updateData();
-        }
-        else if (choice == "exit") {
-            break;
-        }
-        else {
-            cout << "Error: choice not recognized." << endl;
-        }
-    }
+      
+      auto startTimeTrie = chrono::high_resolution_clock::now();
+      //insert into trie
+      auto endTimeTrie = chrono::high_resolution_clock::now();
+      auto durationTrie = chrono::duration_cast<chrono::milliseconds>(endTrie - startTrie).count();
 
+      auto startTimeRB = chrono::high_resolution_clock::now();
+      //insert into red-black
+      auto endTimeRB = chrono::high_resolution_clock::now();
+      auto durationRB = chrono::duration_cast<chrono::milliseconds>(endRB - startRB).count();  
+
+      if (count == 0) {
+        cout << "No matching CVEs found" << endl;
+      } else {
+        cout << "CVEs found:  " << count << endl;
+      }
+      
+    } else {
+      cout << "No command found" << endl;
+    }
+  }
+
+  // cout to user how to format search
+  
+  //searches and printing out
+
+  //time tree searches
+  return 0;
 }
